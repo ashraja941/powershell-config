d79aa5054c83c598ddecf8aa561f0b14a4e8234b
ash <ashraja941@gmail.com>
2025-06-26 20:06:44 -0500

    changed fzf to use zoxide instead of cd

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
-function fcd { cd (fzf --walker=dir --walker-root=.) }
+function fz { z (fzf --walker=dir --walker-root=.) }
