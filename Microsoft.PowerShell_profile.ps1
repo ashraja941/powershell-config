6b14f3e95bfca7418454faf090dffa90e66da7e2
ash <ashraja941@gmail.com>
2025-11-04 18:04:07 -0600

    added comments and changed the omp theme

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,9 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
+# Oh-My-Posh themese select
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\catppuccin_mocha.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
+
+# Initialize Zoxide
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
+# Custom Keybindings
 function fz { z (fzf --walker=dir --walker-root=.) }
