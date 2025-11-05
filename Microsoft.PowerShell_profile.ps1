d106c501c2172a5de6aef1e4f1aa8846e0264913
ash <ashraja941@gmail.com>
2025-11-04 18:20:59 -0600

    changed to catpuccin theme

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 # Oh-My-Posh themese select
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\catppuccin_mocha.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\catppuccin_mocha.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
 
 # Initialize Zoxide
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
